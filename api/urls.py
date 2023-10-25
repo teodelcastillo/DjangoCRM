6dcaef365e181fa25b615bbbee9cd8dfbb6bd2aa
teodelcastillo <teodorodelcastillop@gmail.com>
2023-10-25 18:59:18 -0300

    Urls for all views

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('getClients/', views.getData),
-    path('addClient/', views.addClient)
+    path('addClient/', views.addClient),
+    path('getClientDetail/<int:client_id>/', views.getData),
+    path('updateClient/<int:client_id>/', views.addClient),
+    path('deleteClient/<int:client_id>/', views.addClient)
 ]
