3d100a330fdc4a2a7b98cb7aa38f6651d50f1c3c
teodelcastillo <teodorodelcastillop@gmail.com>
2023-10-25 18:56:17 -0300

    PUT client id delete by client id get by id

--- api/views.py
+++ api/views.py
@@ -4,17 +4,54 @@ from rest_framework import status
 from DjangoProjectManager.models import Client
 from .serializers import ClientSerializer
 
+# Get all clients (GET)
 @api_view(['GET'])
 def getData(request):
     clients = Client.objects.all()
-    serializer = ClientSerializer(clients, many= True)
+    serializer = ClientSerializer(clients, many=True)
     return Response(serializer.data)
 
+# Add a client (POST)
 @api_view(['POST'])
 def addClient(request):
     serializer = ClientSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)  # Código 201 para indicar que se ha creado el recurso
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Manejar errores de validación
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# Get client by ID (GET)
+@api_view(['GET'])
+def getClientDetail(request, client_id):
+    try:
+        client = Client.objects.get(pk=client_id)
+    except Client.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ClientSerializer(client)
+    return Response(serializer.data)
+
+# Update client by ID (PUT)
+@api_view(['PUT'])
+def updateClient(request, client_id):
+    try:
+        client = Client.objects.get(pk=client_id)
+    except Client.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ClientSerializer(client, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Delete client by ID (DELETE)
+@api_view(['DELETE'])
+def deleteClient(request, client_id):
+    try:
+        client = Client.objects.get(pk=client_id)
+    except Client.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    client.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
