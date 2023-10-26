28462736456085f86ef410c38fcf2b6a7c786be5
teodelcastillo <teodorodelcastillop@gmail.com>
2023-10-26 20:19:49 -0300

    Creating project calls

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getClients/', views.getData),
+    path('getClients/', views.getClients),
     path('addClient/', views.addClient),
-    path('getClientDetail/<int:client_id>/', views.getData),
+    path('getClientDetail/<int:client_id>/', views.getClientDetail),
     path('updateClient/<int:client_id>/', views.addClient),
-    path('deleteClient/<int:client_id>/', views.addClient),
-    path('client/<int:pk>/projects/', views.client_projects, name="client_projects"),
+    path('deleteClient/<int:client_id>/', views.deleteClient),
+    path('client/<int:pk>/projects/', views.client_projects),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from DjangoProjectManager.models import Client
+from DjangoProjectManager.models import Client, Project
 from .serializers import ClientSerializer, ProjectSerializer
+
+### CLIENTS ###
 
 # Get all clients (GET)
 @api_view(['GET'])
-def getData(request):
+def getClients(request):
     clients = Client.objects.all()
     serializer = ClientSerializer(clients, many=True)
     return Response(serializer.data)
@@ -68,3 +70,57 @@ def client_projects(request, pk):
     project_serializer = ProjectSerializer(projects, many=True)
 
     return Response(project_serializer.data)
+
+
+### PROJECTS ###
+
+# Get projects (GET)
+@api_view(['GET'])
+def getProjects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many = True)
+    return Response(serializer.data)
+
+# Add a new project (POST)
+def addProject(request):
+    serializer = ProjectSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Update client by ID (PUT)
+@api_view(['PUT'])
+def updateProject(request, project_id):
+    try:
+        project = Project.objects.get(pk=project_id)
+    except Project.DoesNotExist():
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ProjectSerializer(project, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Delete project by ID (DELETE)
+@api_view(['DELETE'])
+def deleteProject(request, project_id):
+    try:
+        project = Project.objects.get(pk=project_id)
+    except Project.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    project.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Get project by ID (GET)
+@api_view(['GET'])
+def getProjectDetail(request, project_id):
+    try:
+        project = Project.objects.get(pk=project_id)
+    except Project.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ProjectSerializer(project)
+    return Response(serializer.data)
