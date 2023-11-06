2efe4731bad9d51e90743ae7fcf755ed31726a1a
teodelcastillo <teodorodelcastillop@gmail.com>
2023-11-06 18:02:39 -0300

    CREATING THE REST OF THE VIEWS

--- api/views.py
+++ api/views.py
@@ -72,3 +72,113 @@ def getProjectsWithAppointments(request):
     projects = Project.objects.filter(appointments__isnull=False).distinct()
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
+
+
+### CLIENTS ###
+
+# Get clients (GET)
+@api_view(['GET'])
+def getClients(request):
+    clients = Client.objects.all()
+    serializer = ClientSerializer(clients, many=True)
+    return Response(serializer.data)
+
+# Add a new client (POST)
+@api_view(['POST'])
+def addClient(request):
+    serializer = ClientSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Update client by ID (PUT)
+@api_view(['PUT'])
+def updateClient(request, client_id):
+    try:
+        client = Client.objects.get(pk=client_id)
+    except Client.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ClientSerializer(client, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Delete client by ID (DELETE)
+@api_view(['DELETE'])
+def deleteClient(request, client_id):
+    try:
+        client = Client.objects.get(pk=client_id)
+    except Client.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    client.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Get client by ID (GET)
+@api_view(['GET'])
+def getClientDetail(request, client_id):
+    try:
+        client = Client.objects.get(pk=client_id)
+    except Client.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ClientSerializer(client)
+    return Response(serializer.data)
+
+
+### APPOINTMENTS ###
+
+# Get appointments (GET)
+@api_view(['GET'])
+def getAppointments(request):
+    appointments = Appointment.objects.all()
+    serializer = AppointmentSerializer(appointments, many=True)
+    return Response(serializer.data)
+
+# Add a new appointment (POST)
+@api_view(['POST'])
+def addAppointment(request):
+    serializer = AppointmentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Update appointment by ID (PUT)
+@api_view(['PUT'])
+def updateAppointment(request, appointment_id):
+    try:
+        appointment = Appointment.objects.get(pk=appointment_id)
+    except Appointment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = AppointmentSerializer(appointment, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Delete appointment by ID (DELETE)
+@api_view(['DELETE'])
+def deleteAppointment(request, appointment_id):
+    try:
+        appointment = Appointment.objects.get(pk=appointment_id)
+    except Appointment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    appointment.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Get appointment by ID (GET)
+@api_view(['GET'])
+def getAppointmentDetail(request, appointment_id):
+    try:
+        appointment = Appointment.objects.get(pk=appointment_id)
+    except Appointment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = AppointmentSerializer(appointment)
+    return Response(serializer.data)
